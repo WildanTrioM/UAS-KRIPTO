9772c1dd82b7ce6370b9e890a38a7258cd678358
WildanTrioM <121679664+WildanTrioM@users.noreply.github.com>
2023-01-18 04:36:55 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+string xorEncryptDecrypt(string toEncrypt, char key) {
+    for (int i = 0; i < toEncrypt.size(); i++) {
+        toEncrypt[i] = toEncrypt[i] ^ key;
+    }
+    return toEncrypt;
+}
+
+int main() {
+    string plaintext = "Selamat dan Sukses Ujian Akhir Semester Gasal 2022-2023";
+    char key = 'wildan';
+    cout << "Plaintext : " << plaintext << endl;
+    string ciphertext = xorEncryptDecrypt(plaintext, key);
+    cout << "Ciphertext : " << ciphertext << endl;
+    string decryptedtext = xorEncryptDecrypt(ciphertext, key);
+    cout << "Decryptedtext : " << decryptedtext << endl;
+    return 0;
+}
+
